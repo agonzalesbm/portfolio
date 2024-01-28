445ac2e809aeb98bb584655893ec6abb86066fb4
agonzalesbm <andres.gonzales.dev@gmail.com>
2024-01-28 12:17:52 -0400

    [@agonzalesbm] Student service create

---
+++ portfolio-backend/dotnet/postgresql-api/Services/IStudentService.cs
@@ -0,0 +1,7 @@
+using postgresql_api.Models;
+
+namespace postgresql_api.Services;
+
+public interface IStudentService : IService<Student>
+{
+}
---
+++ portfolio-backend/dotnet/postgresql-api/Services/StudentService.cs
@@ -0,0 +1,39 @@
+using postgresql_api.Repository;
+using postgresql_api.Models;
+
+namespace postgresql_api.Services;
+
+public class StudentService : IStudentService
+{
+  private readonly IStudentRepository _repository;
+
+  public StudentService(IStudentRepository repostory)
+  {
+    _repository = repostory;
+  }
+
+  public async Task<IEnumerable<Student>> GetAll()
+  {
+    return await _repository.GetAll();
+  }
+
+  public async Task<Student?> Get(int id)
+  {
+    return await _repository.Get(id);
+  }
+
+  public async Task<Student> Create(Student student)
+  {
+    return await _repository.Create(student);
+  }
+
+  public async Task Update(int id, Student student)
+  {
+    await _repository.Update(id, student);
+  }
+
+  public async Task Delete(int id)
+  {
+    await _repository.Delete(id);
+  }
+}
