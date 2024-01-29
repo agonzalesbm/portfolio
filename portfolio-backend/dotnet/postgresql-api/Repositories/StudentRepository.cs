e7b5b7e099c06821093ecdeb400da34b366a8db4
agonzalesbm <andres.gonzales.dev@gmail.com>
2024-01-28 21:17:54 -0400

    [@agonzalesbm] HTTP exception handle on StudentRepostiory

--- portfolio-backend/dotnet/postgresql-api/Repositories/StudentRepository.cs
+++ portfolio-backend/dotnet/postgresql-api/Repositories/StudentRepository.cs
@@ -1,12 +1,18 @@
 using Microsoft.EntityFrameworkCore;
 using postgresql_api.Db;
 using postgresql_api.Models;
+using postgresql_api.Exceptions;
 
 namespace postgresql_api.Repository;
 
 public class StudentRepository : IStudentRepository
 {
   private readonly AppDbContext _context;
+  private readonly int BAD_REQUEST = 400;
+  private readonly int NOT_FOUND = 404;
+  private readonly string INVALID_ID = "Invalid id, should be greater than 0.";
+  private readonly string NOT_FOUND_STUDENT = "Student not found.";
+  private readonly string STUDENT_NULL = "Student not should be null";
 
   public StudentRepository(AppDbContext context)
   {
@@ -20,16 +26,25 @@ public class StudentRepository : IStudentRepository
 
   public async Task<Student?> Get(int id)
   {
+    if (id <= 0)
+    {
+      throw new HttpResponseException(BAD_REQUEST, INVALID_ID);
+    }
+
     var studentItem = await _context.Students.FindAsync(id);
     if (studentItem is null)
     {
-      throw new Exception();
+      throw new HttpResponseException(NOT_FOUND, NOT_FOUND_STUDENT);
     }
     return studentItem;
   }
 
   public async Task<Student> Create(Student student)
   {
+    if (student is null)
+    {
+      throw new HttpResponseException(BAD_REQUEST, STUDENT_NULL);
+    }
     var createStudent = await _context.AddAsync(student);
     await _context.SaveChangesAsync();
     return createStudent.Entity;
@@ -37,10 +52,20 @@ public class StudentRepository : IStudentRepository
 
   public async Task Update(int id, Student student)
   {
+    if (id <= 0)
+    {
+      throw new HttpResponseException(BAD_REQUEST, INVALID_ID);
+    }
+
+    if (student is null)
+    {
+      throw new HttpResponseException(BAD_REQUEST, STUDENT_NULL);
+    }
+
     var studentItem = await _context.Students.FindAsync(id);
     if (studentItem is null)
     {
-      throw new Exception();
+      throw new HttpResponseException(NOT_FOUND, NOT_FOUND_STUDENT);
     }
 
     studentItem.Id = student.Id;
@@ -49,7 +74,6 @@ public class StudentRepository : IStudentRepository
     studentItem.FathersName = student.FathersName;
     studentItem.MothersName = studentItem.MothersName;
 
-    // WARNING: Improve the exceptions
     try
     {
       await _context.SaveChangesAsync();
@@ -62,6 +86,11 @@ public class StudentRepository : IStudentRepository
 
   public async Task Delete(int id)
   {
+    if (id <= 0)
+    {
+      throw new HttpResponseException(BAD_REQUEST, INVALID_ID);
+    }
+
     var studentItem = await _context.Students.FindAsync(id);
     if (studentItem is null)
     {
